cb1804d24d40b79369d3c6938859d9616b691e02
Nitin Chaurasia <gs.nitin@gmail.com>
2019-07-07 02:40:51 -0400

    first commit

---
+++ src/main/java/com/spring5/concepts/Spring5DiConceptsApplication.java
@@ -0,0 +1,13 @@
+package com.spring5.concepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5DiConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5DiConceptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring5/concepts/Spring5DiConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring5.concepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5DiConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
