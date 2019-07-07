fb19c9b1e23feb8111283d95a49d6bd640c88c84
Nitin Chaurasia <gs.nitin@gmail.com>
2019-07-07 03:22:36 -0400

    Creating the context and getting the bean

--- src/main/java/com/spring5/concepts/Spring5DiConceptsApplication.java
+++ src/main/java/com/spring5/concepts/Spring5DiConceptsApplication.java
@@ -2,12 +2,23 @@ package com.spring5.concepts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.spring5.concepts.controllers.MyController;
 
 @SpringBootApplication
 public class Spring5DiConceptsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5DiConceptsApplication.class, args);
+		
+		//Getting in the context of the 
+		ApplicationContext ctx = SpringApplication.run(Spring5DiConceptsApplication.class, args);
+		
+		//get the bean by ClassName but the first letter as small
+		//The Bean is created by the Spring Context handler and we dont have to instantiate a new bean object
+		MyController controller = (MyController) ctx.getBean("myController");
+		controller.hello();
+		
 	}
 
 }
