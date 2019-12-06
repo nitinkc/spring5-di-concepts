422481f73ed6ec66ce3b365968afdf6ce200da29
Nitin Chaurasia <gs.nitin@gmail.com>
2019-12-06 00:11:22 -0500

    Bean Life cycle demo

---
+++ src/main/java/com/spring5/concepts/CustomBeanPostProcessor.java
@@ -0,0 +1,27 @@
+package com.spring5.concepts;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+        if (bean instanceof BeanLifeCycleDemo) {
+            ((BeanLifeCycleDemo) bean).beforeInit();
+        }
+
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (bean instanceof BeanLifeCycleDemo) {
+            ((BeanLifeCycleDemo) bean).afterInit();
+        }
+
+        return bean;
+    }
+}
